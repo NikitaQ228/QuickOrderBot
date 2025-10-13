84ffa549970831261b4dfa6e5929ea8f08aa078b
Nikita Baboshin <baboshinikki90@gmail.com>
2025-10-13 14:33:13 +0400

    Init commit

---
+++ src/main/java/ru/nikita/QuickOrderBot/QuickOrderBotApplication.java
@@ -0,0 +1,13 @@
+package ru.nikita.QuickOrderBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickOrderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickOrderBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nikita/QuickOrderBot/QuickOrderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikita.QuickOrderBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickOrderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
