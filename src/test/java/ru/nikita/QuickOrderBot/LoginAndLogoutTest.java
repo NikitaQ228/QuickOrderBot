aa1f04e318c006ffc95af20dd0f0fb0e2d2774fe
Nikita Baboshin <baboshinikki90@gmail.com>
2025-11-25 17:38:43 +0400

    fix: login/logout test

--- src/test/java/ru/nikita/QuickOrderBot/LoginAndLogoutTest.java
+++ src/test/java/ru/nikita/QuickOrderBot/LoginAndLogoutTest.java
@@ -9,14 +9,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginAndLogoutTest {
     private static WebDriver driver;
+
+    @LocalServerPort
+    private int port;
 
     @BeforeAll
     public static void setUp() {
@@ -26,7 +33,7 @@ public class LoginAndLogoutTest {
 
     @Test
     public void testLoginAndLogout() {
-        driver.get("http://localhost:8080/login");
+        driver.get("http://localhost:" + port + "/login");
 
         WebElement usernameInput = driver.findElement(By.name("username"));
         WebElement passwordInput = driver.findElement(By.name("password"));
@@ -43,10 +50,10 @@ public class LoginAndLogoutTest {
 
         new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(d -> d.getCurrentUrl().contains("/login")
-                        || d.getCurrentUrl().equals("http://localhost:8080/"));
+                        || d.getCurrentUrl().equals("http://localhost:" + port + "/"));
 
         assertTrue(driver.getCurrentUrl().contains("/login")
-                || driver.getCurrentUrl().equals("http://localhost:8080/"));
+                || driver.getCurrentUrl().equals("http://localhost:" + port + "/"));
 
         assertFalse(driver.getPageSource().contains("Выйти"));
     }
